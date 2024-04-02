7836613503286daa6609b3337ddaff122d29cc60
fumtas1k <fumta01a7@gmail.com>
2024-04-02 23:16:56 +0900

    Initial commit

---
+++ test/notification_cloudwatch_alarm.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NotificationCloudwatchAlarm from '../lib/notification_cloudwatch_alarm-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/notification_cloudwatch_alarm-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NotificationCloudwatchAlarm.NotificationCloudwatchAlarmStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
